bf6b48d6f9a1115de1fd50581d980f1ee85a7267
Vumirka <rugkevuhvictoria@gmail.com>
2026-02-10 10:51:48 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,23 @@
 package domain;
-
+/**
+ * Клас для обчислення кількості номерів бойової техніки,
+ * які містять "нещасливі" значення: цифру 4 або число 13.
+ * Вхідні дані:
+ * maxNumber - максимальний номер техніки (100000).
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static int Calculate(int maxNumber) {
+        int excludedCount = 0;
+
+        for (int i = 1; i <= maxNumber; i++) {
+            String number = String.valueOf(i);
+
+            if (number.contains("4") || number.contains("13")) {
+                excludedCount++;
+            }
+        }
+
+        return excludedCount;
     }
 }
